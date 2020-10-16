d3cde01b9d3e2d90bfb90c6ebb44ca71a4df02c1
Anastasiia Gorbenko <nstgorbenko@yandex.ru>
2020-10-16 23:28:44 +0300

    feat: implement balanced brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const stack = [];
+
+    for (let i = 0; i < str.length; i++) {
+        const bracket = str[i];
+
+        const openingBracketIndex = bracketsConfig.findIndex((config) => bracket === config[0]);
+        const closingBracketIndex = bracketsConfig.findIndex((config) => bracket === config[1]);
+
+        const isOpeningBracket = openingBracketIndex !== -1;
+        const isClosingBracket = closingBracketIndex !== -1;
+        const isStackTopMatchClosingBracket = isClosingBracket && stack[stack.length - 1] === bracketsConfig[closingBracketIndex][0];
+
+        if (!isOpeningBracket && !isClosingBracket) {
+          continue;
+        }
+
+        if (isClosingBracket && isStackTopMatchClosingBracket) {
+            stack.pop();
+
+        } else if (isOpeningBracket) {
+            stack.push(bracket);
+
+        } else if (isClosingBracket && !isStackTopMatchClosingBracket) {
+          return false;
+        }
+    }
+
+    return stack.length === 0;
 }
